4803220fb68effa24bfe062aee9889f8421fa50f
livecodelife <calebcowen@gmail.com>
2022-04-19 10:27:11 -0500

    setup for class component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
+import React, {Component} from 'react'
 
-function App() {
-  const todos = ['a todo', 'another todo', 'a third todo']
+class App extends Component() {
+  render(){
+    const todos = ['a todo', 'another todo', 'a third todo']
 
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <ul>
-        {todos.map((todo) => {
-          return (
-            <li>
-              {todo}
-            </li>
-          )
-        })}
-      </ul>
-    </div>
-  );
+    return (
+      <div>
+        <h1>My Todos</h1>
+        <ul>
+          {todos.map((todo) => {
+            return (
+              <li>
+                {todo}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
