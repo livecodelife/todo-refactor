7969322664d14b0d14380b64e43b1e99ab3a735d
livecodelife <calebcowen@gmail.com>
2022-04-19 10:36:40 -0500

    sets up component state refactor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
+import { Component } from 'react'
 
-function App() {
-  const todos = ['a todo', 'another todo', 'a third todo']
+class App extends Component() {
+  constructor(){
+    super()
+    this.state = {
+      todos: []
+    }
+  }
 
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <ul>
-        {todos.map((todo) => {
-          return (
-            <li>
-              {todo}
-            </li>
-          )
-        })}
-      </ul>
-    </div>
-  );
+  componentDidMount() {
+    const newTodos = [
+      'a todo', 
+      'another todo', 
+      'a third todo'
+    ]
+
+    this.setState({...this.state, todos: newTodos})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My Todos</h1>
+        <ul>
+          {this.state.todos.map((todo) => {
+            return (
+              <li>
+                {todo}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
